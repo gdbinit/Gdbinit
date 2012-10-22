1075a0ea1206ac85a3d0c369f24f2e8dd84780a8
David Holm <david.holm@ericsson.com>
2012-10-22 13:47:53 +0200

    Adds support for MIPS
    Restructures the architecture-specific code to never assume that a
    certain architecture is being used. Adds a $MIPS global that when set to
    1 identifies that the debugged target is MIPS.
    
    Also breaks out the script for detecting a target and places it as a
    bash-script in the .gdb-directory to make it easier to debug.

---
+++ .gdb/detect-target.sh
@@ -0,0 +1,26 @@
+#!/bin/env bash
+
+TARGET_DOUBLET=$(grep 'file type' /tmp/gdb_info_target |
+                 sed 's/\.$//g' |
+                 cut -d ' ' -f 4 |
+                 uniq |
+                 tr -d '\n')
+GDB_FILE="/tmp/gdb_target_arch.gdb"
+rm -f "$GDB_FILE"
+
+case "$TARGET_DOUBLET" in
+    *-i386)
+        echo "set \$X86 = 1" >> $GDB_FILE;
+        ;;
+    *-x86-64)
+        echo "set \$X86_64 = 1" >> $GDB_FILE;
+        echo "set \$64BITS = 1" >> $GDB_FILE;
+        ;;
+    *-arm*)
+        echo "set \$ARM = 1" >> $GDB_FILE;
+        ;;
+    *-*mips*)
+        echo "set \$MIPS = 1" >> $GDB_FILE;
+        echo "set \$64BITS = 1" >> $GDB_FILE;
+        ;;
+esac
