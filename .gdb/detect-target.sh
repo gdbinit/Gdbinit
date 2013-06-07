70a61254396f4f4c3d39125cda58b7bbddd2c8dd
John Szakmeister <john@szakmeister.net>
2013-06-06 22:54:57 -0400

    Attempt to detect the correct processor type under Snow Leopard's gdb.
    Unfortunately, gdb under Snow Leopard doesn't give a useful doublet.  So
    let's go the extra step and grab the osabi and use it to determine what
    processor we're using and the size.

--- .gdb/detect-target.sh
+++ .gdb/detect-target.sh
@@ -5,6 +5,9 @@ TARGET_DOUBLET=$(grep 'file type' /tmp/gdb_info_target |
                  cut -d ' ' -f 4 |
                  uniq |
                  tr -d '\n')
+OSABI=$(grep 'currently ' /tmp/gdb_info_target |
+        sed 's/.*currently "\([^"]*\)".*/\1/' |
+        tr -d '\n')
 GDB_FILE="/tmp/gdb_target_arch.gdb"
 rm -f "$GDB_FILE"
 
@@ -22,5 +25,13 @@ case "$TARGET_DOUBLET" in
     *-*mips*)
         echo "set \$MIPS = 1" >> $GDB_FILE;
         echo "set \$64BITS = 1" >> $GDB_FILE;
+        ;;
+    mach-o-*)
+        if test "$OSABI" == "Darwin64"; then
+            echo "set \$X86_64 = 1" >> $GDB_FILE;
+            echo "set \$64BITS = 1" >> $GDB_FILE;
+        elif test "$OSABI" == "Darwin"; then
+            echo "set \$X86 = 1" >> $GDB_FILE;
+        fi
         ;;
 esac
